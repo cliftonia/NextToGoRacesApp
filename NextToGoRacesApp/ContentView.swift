d8d8c35257a438fcc63df75d43c97a06d47a09d5
cliftonia <clifton.baggerman@me.com>
2025-02-10 19:25:32 +1000

    Initial Commit

---
+++ NextToGoRacesApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NextToGoRacesApp
+//
+//  Created by Clifton Baggerman on 10/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NextToGoRacesApp/NextToGoRacesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextToGoRacesAppApp.swift
+//  NextToGoRacesApp
+//
+//  Created by Clifton Baggerman on 10/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct NextToGoRacesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
